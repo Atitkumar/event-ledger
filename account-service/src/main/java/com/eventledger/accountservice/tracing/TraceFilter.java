8146bbd812dd886ee0f3012aa536c9e713390c21
Atit <amachhirke6@gmail.com>
2026-06-18 17:06:29 +0530

    "Trace filter"

--- account-service/src/main/java/com/eventledger/accountservice/tracing/TraceFilter.java
+++ account-service/src/main/java/com/eventledger/accountservice/tracing/TraceFilter.java
@@ -1,13 +1,11 @@
-package com.eventledger.gateway.tracing;
+package com.eventledger.accountservice.tracing;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @Component
 public class TraceFilter implements Filter {
@@ -22,27 +20,20 @@ public class TraceFilter implements Filter {
         HttpServletRequest httpRequest =
                 (HttpServletRequest) request;
 
-        HttpServletResponse httpResponse =
-                (HttpServletResponse) response;
-
         String traceId =
-                httpRequest.getHeader("X-Trace-Id");
+                httpRequest.getHeader(
+                        "X-Trace-Id"
+                );
 
-        if (traceId == null || traceId.isBlank()) {
+        if (traceId != null) {
 
-            traceId =
-                    UUID.randomUUID()
-                            .toString();
+            TraceContext.setTraceId(traceId);
+
+            MDC.put(
+                    "traceId",
+                    traceId
+            );
         }
-
-        TraceContext.setTraceId(traceId);
-
-        MDC.put("traceId", traceId);
-
-        httpResponse.setHeader(
-                "X-Trace-Id",
-                traceId
-        );
 
         try {
 
